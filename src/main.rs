6283b16aaf4215914f8b9f67012cdd7e2c83e0a8
ShaddyBear🐻 <124757807+shaddybear@users.noreply.github.com>
2025-08-08 03:15:39 +0530

    [new rustyy]: add overflow & boolean basics rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, Rust!, Im gaurav");
+     let is_male: bool = true;
+     let is_above_18:  bool = true;
+
+     if is_male {
+        println!("You are a male");
+     }
+
+     else {
+        println!("You are a female");
+     }
+     if is_male && is_above_18 {
+         println!("You are a legal male");
+     }
 }
